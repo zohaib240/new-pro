bc27abefee7c480a5daca0e221c9bfc2db8cad38
Zohaib Asghar <155531422+zohaib240@users.noreply.github.com>
2024-06-03 09:44:30 +0500

    Add files via upload

---
+++ Bulb-Assignment/app.js
@@ -0,0 +1,11 @@
+const img =document.querySelector('img')
+img.addEventListener('mouseover',function () {
+
+    img.src='https://freepngimg.com/thumb/bulb/4-yellow-light-bulb-png-image-thumb.png'
+    
+})
+
+img.addEventListener('mouseout',function () {
+    img.src='https://w7.pngwing.com/pngs/859/425/png-transparent-light-bulb-illustration-incandescent-light-bulb-lamp-bulb-candle-product-light-thumbnail.png'
+    
+})
